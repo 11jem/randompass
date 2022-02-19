6acdbcf17111fea97e29ff3ac4227eb33361fda2
11jem <cabalidajem@gmail.com>
2022-02-19 00:23:07 -0600

    refactored utils

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -3,12 +3,21 @@ const numbers = '0123456789';
 const symbols = '~!@#$%^&*_+=-';
 
 const finalizePassword = (length, chars) => {
-  let password = '';
+  // let password = '';
 
-  for (let i = 0; i < length; i++) {
-    password += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return password;
+  // for (let i = 0; i < length; i++) {
+  //   password += chars.charAt(Math.floor(Math.random() * chars.length));
+  // }
+  // return password;
+
+  return chars
+    .split('')
+    .reduce(
+      (password, el, i, arr) =>
+        (password += arr[Math.floor(Math.random() * arr.length)]),
+      ''
+    )
+    .slice(0, length);
 };
 
 const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
